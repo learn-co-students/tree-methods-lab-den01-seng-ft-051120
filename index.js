24bda893a7e9603c59992de4addbba95203b3f2c
Bryce Kennedy <btken88@gmail.com>
2020-09-16 11:06:58 -0600

    Complete tree max method

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 function inOrder(currentNode) {
   if (currentNode.left) {
     inOrder(currentNode.left);
-  };
+  }
   console.log(currentNode.data);
   if (currentNode.right) {
     inOrder(currentNode.right);
-  };
-};
+  }
+}
 
 function findOrAdd(root, newNode) {
   let currentNode = root;
   if (currentNode.data === newNode.data) {
-    return true
+    return true;
   } else if (currentNode.data < newNode.data) {
     if (!currentNode.right) {
-      return currentNode.right = newNode
+      return currentNode.right = newNode;
     }
-    currentNode = currentNode.right
-    return findOrAdd(currentNode, newNode)
+    currentNode = currentNode.right;
+    return findOrAdd(currentNode, newNode);
   } else if (currentNode.data > newNode.data) {
     if (!currentNode.left) {
-      return currentNode.left = newNode
+      return currentNode.left = newNode;
     }
-    currentNode = currentNode.left
-    return findOrAdd(currentNode, newNode)
+    currentNode = currentNode.left;
+    return findOrAdd(currentNode, newNode);
   }
 }
 
+function max(node) {
+  if (node.right) {
+    return max(node.right)
+  }
+  return node
+}
